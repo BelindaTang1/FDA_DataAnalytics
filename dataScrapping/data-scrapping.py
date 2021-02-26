ca5bfd1bae9d71ad388196a75a83d1b703d3d46a
BelindaTangN <74804068+BelindaTangN@users.noreply.github.com>
2021-02-26 16:17:21 +0800

    Add files via upload

---
+++ dataScrapping/data-scrapping.py
@@ -0,0 +1,36 @@
+''' 
+This is a funcaitonal file only to download Establishment Registration and Medical Device Listing Files from FDA ; and also can download released 510k zip files from FDA. 
+These files are updated monthly on the 5th of each month on fda website, therefore, the system will update on the 6th to make sure the latest info can be used for the text builder.
+'''
+import json
+import requests
+import os
+import sys
+from zipfile import ZipFile
+import io
+import time
+
+# Configure
+INFO_FILE = "fda_database.json"
+ZIP_FILES_URL_EST = "zip-files-url" 
+ZIP_FILES_URL_510K = "510k-zip-file-url"
+
+DIRECTORY_FOR_EST = "FDA Registration Record/EST"
+DIRECTORY_FOR_510K = "FDA Registration Record/510k"
+
+class data_scrapper_510k(object):
+    def __init__(self):
+        self.config_json = INFO_FILE
+        self.download_url = ZIP_FILES_URL_510K
+        self.directory = DIRECTORY_FOR_510K
+
+    def scrap(self):
+        with open(os.path.join(sys.path[0],self.config_json), "r") as JSON_FILE:
+            data = json.load(JSON_FILE)
+            url = data[self.download_url]
+
+        for i in url.keys():
+            r = requests.get(url[i], stream = True)
+            z = ZipFile(io.BytesIO(r.content))
+            z.extractall(self.directory)
+
