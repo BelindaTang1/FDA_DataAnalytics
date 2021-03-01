d523af370ab4ff1e219fc43761ce947efe2b4b97
BelindaTangN <74804068+BelindaTangN@users.noreply.github.com>
2021-03-01 16:43:29 +0800

    Enter

--- dataScrapping/__init__.py
+++ dataScrapping/__init__.py
@@ -1,2 +1,9 @@
-import data-scrapping
+import sys
+import os 
+ABS_PATH = os.path.abspath('data_scrapping.py')
+sys.path.append(ABS_PATH)
+
+import data_scrapping
+
+print(data_scrapping.INFO_FILE)
 
